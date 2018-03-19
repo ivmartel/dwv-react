f53ee63372f6d5e8e41dacdca02c46b4af6cf2c4
ivmartel <ivmartel@gmail.com>
2018-03-20 00:29:29 +0100

    Adapted demo app to run dwv.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import DwvApp from './DwvApp';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<DwvApp />, document.getElementById('root'));
 registerServiceWorker();
