18e9b1ddb978d4f280af14fe783f727551c62e6c
ivmartel <ivmartel@gmail.com>
2018-03-20 21:38:47 +0100

    Prepare gh-pages branch.

--- src/DwvApp.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DwvApp from './DwvApp';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<DwvApp />, div);
-});
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import DwvApp from './DwvApp';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<DwvApp />, document.getElementById('root'));
-registerServiceWorker();
