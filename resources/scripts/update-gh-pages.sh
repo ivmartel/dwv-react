19f18d34855c044b930904bfdba765b2ba4d62ef
ivmartel <ivmartel@gmail.com>
2018-03-21 22:39:15 +0100

    Exit if build fails and clean up before copy

--- resources/scripts/update-gh-pages.sh
+++ resources/scripts/update-gh-pages.sh
@@ -6,7 +6,7 @@ echo -e "Starting to update gh-pages\n"
 
 # build deploy version (result in ./build)
 # base href is set in package.json at homepage
-yarn run build
+yarn run build || exit 1
 
 # go to home and setup git
 cd $HOME
@@ -14,6 +14,8 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 # using token, clone gh-pages branch
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ivmartel/dwv-react.git gh-pages
+# clean up demo
+rm -Rf $HOME/gh-pages/demo/trunk/*
 # copy new build in demo/trunk
 cp -Rf $HOME/build/ivmartel/dwv-react/build/* $HOME/gh-pages/demo/trunk
 # move back to root of repo
