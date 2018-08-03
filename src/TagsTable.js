d396ecf24569af0df732bc89ef90783e395d3064
ivmartel <ivmartel@gmail.com>
2018-08-04 00:20:43 +0200

    Remove unused packages.

--- src/TagsTable.js
+++ src/TagsTable.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 
-import Button from '@material-ui/core/Button';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import TextField from '@material-ui/core/TextField';
 
