4eb37ab2b2acbcb501ab2fc76dff77abac8b9809
ivmartel <ivmartel@gmail.com>
2018-08-04 00:11:59 +0200

    Add tools menu. Change tags dialog to table.

--- TagsTable.js
+++ TagsTable.js
@@ -5,14 +5,8 @@ import Typography from '@material-ui/core/Typography';
 
 import Button from '@material-ui/core/Button';
 import InputAdornment from '@material-ui/core/InputAdornment';
-import Dialog from '@material-ui/core/Dialog';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Slide from '@material-ui/core/Slide';
 import TextField from '@material-ui/core/TextField';
 
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
 import Search from '@material-ui/icons/Search';
 
 import Table from '@material-ui/core/Table';
@@ -23,9 +17,6 @@ import TableRow from '@material-ui/core/TableRow';
 import TablePagination from '@material-ui/core/TablePagination';
 
 const styles = theme => ({
-  appBar: {
-    position: 'relative',
-  },
   flex: {
     flex: 1,
   },
@@ -37,19 +28,13 @@ const styles = theme => ({
       backgroundColor: theme.palette.background.default,
     },
   },
-  title: {
-    flex: '0 0 auto',
-  },
   searchField: {
     backgroundColor: 'white',
+    marginLeft: 20
   }
 });
 
-function Transition(props) {
-  return <Slide direction="up" {...props} />;
-}
-
-class TagsDialog extends React.Component {
+class TagsTable extends React.Component {
 
   constructor(props) {
     super(props);
@@ -57,44 +42,35 @@ class TagsDialog extends React.Component {
       data: this.props.data,
       displayData: this.props.data,
       searchfor: "",
-      open: false,
       page: 0,
       rowsPerPage: 10
     };
-    
+
     // bind listener
     this.filterList = this.filterList.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
-      this.setState({
-          data: nextProps.data, 
-          displayData: nextProps.data
-      });
+    this.setState({
+      data: nextProps.data,
+      displayData: nextProps.data
+    });
   }
 
-  handleClickOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleClose = () => {
-    this.setState({ open: false });
-  }
-  
   filterList(event) {
     var search = event.target.value
     var searchLo = search.toLowerCase();
     var updatedList = this.state.data.filter( function (item) {
       for ( var key in item ) {
-          if( item.hasOwnProperty(key) ) {
-              var value = item[key];
-              if ( typeof value === "number" ) {
-                  value = value.toString();
-              }
-              if ( value.toLowerCase().indexOf(searchLo) !== -1 ) {
-                  return true;
-              }
+        if( item.hasOwnProperty(key) ) {
+          var value = item[key];
+          if ( typeof value === "number" ) {
+            value = value.toString();
           }
+          if ( value.toLowerCase().indexOf(searchLo) !== -1 ) {
+            return true;
+          }
+        }
       }
       return false;
     });
@@ -104,7 +80,7 @@ class TagsDialog extends React.Component {
   handleChangePage = (event, page) => {
     this.setState({ page });
   }
-  
+
   handleChangeRowsPerPage = event => {
     this.setState({ rowsPerPage: event.target.value });
   }
@@ -112,42 +88,25 @@ class TagsDialog extends React.Component {
   render() {
     const { classes } = this.props;
     const { displayData, searchfor, rowsPerPage, page } = this.state;
-    
+
     return (
-      <div>
-      <Button variant="raised" color="primary" onClick={this.handleClickOpen}>DICOM Tags</Button>
-      <Dialog
-          fullScreen
-          open={this.state.open}
-          onClose={this.handleClose}
-          TransitionComponent={Transition}
-        >
-          <AppBar className={classes.appBar}>
-            <Toolbar>
-              <IconButton color="inherit" onClick={this.handleClose} aria-label="Close">
-                <CloseIcon />
-              </IconButton>
-              <Typography variant="title" color="inherit" className={classes.flex}>
-                DICOM Tags
-              </Typography>
-              <TextField
-                id="search"
-                type="search"
-                value={searchfor}
-                className={classes.searchField}
-                onChange={this.filterList}
-                margin="normal"
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <Search />
-                    </InputAdornment>
-                  )
-                }}
-              />
-            </Toolbar>
-          </AppBar>
-          
+      <div className={classes.container}>
+        <TextField
+          id="search"
+          type="search"
+          value={searchfor}
+          className={classes.searchField}
+          onChange={this.filterList}
+          margin="normal"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Search />
+              </InputAdornment>
+            )
+          }}
+        />
+
         <Table className={classes.table}>
           <TableHead>
             <TableRow>
@@ -156,7 +115,7 @@ class TagsDialog extends React.Component {
             </TableRow>
           </TableHead>
           <TableBody>
-          {displayData.slice(page * rowsPerPage, 
+          {displayData.slice(page * rowsPerPage,
                 page * rowsPerPage + rowsPerPage).map( item => {
             return (
               <TableRow className={classes.row} key={item.group+item.element}>
@@ -183,14 +142,13 @@ class TagsDialog extends React.Component {
           onChangeRowsPerPage={this.handleChangeRowsPerPage}
         />
 
-      </Dialog>
       </div>
     );
   }
 }
 
-TagsDialog.propTypes = {
+TagsTable.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(TagsDialog);
+export default withStyles(styles)(TagsTable);
--- src/DwvApp.js
+++ src/DwvApp.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
 import Button from '@material-ui/core/Button';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import TagsDialog from './TagsDialog';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import Slide from '@material-ui/core/Slide';
+import Toolbar from '@material-ui/core/Toolbar';
+
+import TagsTable from './TagsTable';
 
 import './DwvApp.css';
 import dwv from 'dwv';
-
-// button margin
-const styles = {
-  button: {
-    margin: 2,
-  }
-};
 
 // gui overrides
 
@@ -33,34 +42,106 @@ dwv.image.decoderScripts = {
     "jpeg-baseline": "assets/dwv/decoders/pdfjs/decode-jpegbaseline.js"
 };
 
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  appBar: {
+    position: 'relative',
+  },
+  title: {
+    flex: '0 0 auto',
+  },
+  tagsDialog: {
+    minHeight: '90vh', maxHeight: '90vh',
+    minWidth: '90vw', maxWidth: '90vw',
+  },
+  iconSmall: {
+    fontSize: 20,
+  },
+});
+
+function TransitionUp(props) {
+  return <Slide direction="up" {...props} />;
+}
+
 class DwvApp extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
       legend: 'Powered by dwv ' + dwv.getVersion() + ' and React ' + React.version,
-      loaded: 0,
-      tags: []
+      tools: ['Scroll', 'ZoomAndPan', 'WindowLevel', 'Draw'],
+      selectedTool: 'Select Tool',
+      loadProgress: 0,
+      dataLoaded: false,
+      tags: [],
+      showDicomTags: false,
+      toolMenuAnchorEl: null
     };
   }
 
   render() {
+    const { classes } = this.props;
+    const { legend, tools, loadProgress, dataLoaded, tags, toolMenuAnchorEl } = this.state;
+
+    const toolsMenuItems = tools.map( (tool) =>
+      <MenuItem onClick={this.handleMenuItemClick.bind(this, tool)} key={tool} value={tool}>{tool}</MenuItem>
+    );
+
     return (
       <div id="dwv">
-        <LinearProgress variant="determinate" value={this.state.loaded} />
+        <LinearProgress variant="determinate" value={loadProgress} />
         <div className="button-row">
-          <Button variant="raised" color="primary" value="Scroll" onClick={this.onClick.bind(this)} style={styles.button}>Scroll</Button>
-          <Button variant="raised" color="primary" value="WindowLevel" onClick={this.onClick.bind(this)} style={styles.button}>WindowLevel</Button>
-          <Button variant="raised" color="primary" value="ZoomAndPan" onClick={this.onClick.bind(this)} style={styles.button}>ZoomAndPan</Button>
-          <Button variant="raised" color="primary" value="Draw" onClick={this.onClick.bind(this)} style={styles.button}>Draw</Button>
-          <TagsDialog data={this.state.tags} />
+          <Button variant="raised" color="primary"
+            aria-owns={toolMenuAnchorEl ? 'simple-menu' : null}
+            aria-haspopup="true"
+            onClick={this.handleMenuButtonClick}
+            disabled={!dataLoaded}
+            className={classes.button}
+            size="medium"
+          >{ this.state.selectedTool }
+          <ArrowDropDownIcon className={classes.iconSmall}/></Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={toolMenuAnchorEl}
+            open={Boolean(toolMenuAnchorEl)}
+            onClose={this.handleMenuClose}
+          >
+            {toolsMenuItems}
+          </Menu>
+
+          <Button variant="raised" color="primary"
+            onClick={this.handleTagsDialogOpen}
+            disabled={!dataLoaded}
+            className={classes.button}
+            size="medium">Tags</Button>
+          <Dialog
+            open={this.state.showDicomTags}
+            onClose={this.handleTagsDialogClose}
+            TransitionComponent={TransitionUp}
+            classes={{ paper: classes.tagsDialog }}
+            >
+              <AppBar className={classes.appBar}>
+                <Toolbar>
+                  <IconButton color="inherit" onClick={this.handleTagsDialogClose} aria-label="Close">
+                    <CloseIcon />
+                  </IconButton>
+                  <Typography variant="title" color="inherit" className={classes.flex}>
+                    DICOM Tags
+                  </Typography>
+                </Toolbar>
+              </AppBar>
+              <TagsTable data={tags} />
+          </Dialog>
         </div>
+
         <div className="layerContainer">
-          <div className="dropBox"></div>
+          <div className="dropBox">Drag and drop data here.</div>
           <canvas className="imageLayer">Only for HTML5 compatible browsers...</canvas>
           <div className="drawDiv"></div>
         </div>
-        <div className="legend">{this.state.legend}</div>
+        <div className="legend">{legend}</div>
       </div>
     );
   }
@@ -72,28 +153,63 @@ class DwvApp extends React.Component {
     app.init({
       "containerDivId": "dwv",
       "fitToWindow": true,
-      "tools": ["Scroll", "ZoomAndPan", "WindowLevel", "Draw"],
+      "tools": this.state.tools,
       "shapes": ["Ruler"],
       "isMobile": true
     });
     // progress
     var self = this;
     app.addEventListener("load-progress", function (event) {
-      self.setState({loaded: event.loaded});
+      self.setState({loadProgress: event.loaded});
     });
     app.addEventListener("load-end", function (event) {
+      // set data loaded flag
+      self.setState({dataLoaded: true});
+      // set dicom tags
       self.setState({tags: app.getTags()});
+      // set the selected tool
+      if (app.isMonoSliceData() && app.getImage().getNumberOfFrames() === 1) {
+        self.setState({selectedTool: 'ZoomAndPan'});
+      } else {
+        self.setState({selectedTool: 'Scroll'});
+      }
     });
     // store
     this.setState({dwvApp: app});
   }
 
-  onClick(event) {
+  onChangeTool = tool => {
     if ( this.state.dwvApp ) {
-      this.state.dwvApp.onChangeTool(event);
+      this.setState({selectedTool: tool});
+      this.state.dwvApp.onChangeTool({currentTarget: { value: tool } });
     }
   }
 
+  handleTagsDialogOpen = () => {
+    this.setState({ showDicomTags: true });
+  };
+
+  handleTagsDialogClose = () => {
+    this.setState({ showDicomTags: false });
+  };
+
+  handleMenuButtonClick = event => {
+    this.setState({ toolMenuAnchorEl: event.currentTarget });
+  };
+
+  handleMenuClose = event => {
+    this.setState({ toolMenuAnchorEl: null });
+  };
+
+  handleMenuItemClick = tool => {
+    this.setState({ toolMenuAnchorEl: null });
+    this.onChangeTool(tool);
+  };
+
 }
 
-export default DwvApp;
+DwvApp.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(DwvApp);
