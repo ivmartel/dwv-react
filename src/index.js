dba807218e75113593bc9b5e1857092aad9d9b37
ivmartel <ivmartel@gmail.com>
2018-08-22 23:45:26 +0200

    Reinstate app as the main component.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import DwvApp from './DwvApp';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<DwvApp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
