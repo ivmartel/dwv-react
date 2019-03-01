f3783b40a274c1356afe3f3ec06c0e590911fe75
Harshit Agarwal <harshit.agarwal@synapsica.com>
2019-03-01 14:05:52 +0530

    Suggestions functionality along with TagsCounter added

--- server/server.js
+++ server/server.js
@@ -30,20 +30,39 @@ app.get("/suggestions",async function(req,res){
 })
 
 app.post("/suggestions",async function(req,res){
-    var obj;
+    var suggestion, suggestions, tagsCounter, caseTags,i,p,result=[],obj={};
+    caseTags = req.body.suggestions
     await fs.readFile('server/tmp/suggestions.json', function (err,data) {
         if (err) throw err;
-        obj = JSON.parse(data);
-        suggestions = obj.suggestions
+        suggestion = JSON.parse(data);
+        suggestions = suggestion.suggestions
+        // suggestions = Array.from(new Set(suggestions.concat(caseTags)))
+        for(i=0;i<suggestions.length;i++)obj[suggestions[i].id]=suggestions[i].text;
+        for(i=0;i<caseTags.length;i++)obj[caseTags[i].id]=caseTags[i].text;
+        for(p in obj)if(obj.hasOwnProperty(p))result.push({id:p,text:obj[p]});
+        suggestion.suggestions = result;
+       
+        try{
+            fs.writeFileSync('server/tmp/suggestions.json',JSON.stringify(suggestion))
+            res.send(200, "Suggestions saved successfully")
+        }catch(err){
+            console.log(err);
+            res.send(500, err,"Something went wrong");
+        }
     });
-    console.log(req.body)
-    var data = JSON.stringify(req.body)
-    console.log(data);
-    try{
-        await fs.writeFileSync('server/tmp/suggestions.json',data)
-        res.send(200, "Suggestions saved successfully")
-    }catch(err){
-        console.log(err);
-        res.send(500, err,"Something went wrong");
-    }
+
+    await fs.readFile('server/tmp/tagsCounter.json', function (err,data) {
+        if (err) throw err;
+        tagsCounter = JSON.parse(data);
+        for(i=0; i<caseTags.length; i++){
+            if(tagsCounter.hasOwnProperty(caseTags[i].id)){
+                tagsCounter[caseTags[i].id] += 1;
+            }else{
+                tagsCounter[caseTags[i].id] = 1;
+            }
+        }
+        fs.writeFileSync('server/tmp/tagsCounter.json', JSON.stringify(tagsCounter));
+    });
+    
+    
 })
