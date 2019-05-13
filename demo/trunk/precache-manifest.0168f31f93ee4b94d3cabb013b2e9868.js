5736b7bd3fb68030bd3412be2a5bf6bab9384c8c
Travis <travis@travis-ci.org>
2019-05-13 20:29:46 +0000

    Travis build 494 pushed to gh-pages

---
+++ demo/trunk/precache-manifest.0168f31f93ee4b94d3cabb013b2e9868.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c2763f5a09f168aa69e0672e5a8c6b",
+    "url": "/dwv-react/demo/trunk/index.html"
+  },
+  {
+    "revision": "ff4c53e1aac32b8f3197",
+    "url": "/dwv-react/demo/trunk/static/css/main.68c023de.chunk.css"
+  },
+  {
+    "revision": "089abb60e49ed3413429",
+    "url": "/dwv-react/demo/trunk/static/js/2.cc351ca5.chunk.js"
+  },
+  {
+    "revision": "ff4c53e1aac32b8f3197",
+    "url": "/dwv-react/demo/trunk/static/js/main.4581722c.chunk.js"
+  },
+  {
+    "revision": "9653790fbf10b5aa9de6",
+    "url": "/dwv-react/demo/trunk/static/js/runtime~main.424b9d46.js"
+  }
+]);
--- demo/trunk/service-worker.js
+++ demo/trunk/service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dwv-react/demo/trunk/precache-manifest.1ebf51283be7b73242f71c2cd5928911.js"
+  "/dwv-react/demo/trunk/precache-manifest.0168f31f93ee4b94d3cabb013b2e9868.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/dwv-react/demo/trunk/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dwv-react/demo/trunk/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
