9872499ef0a149d47ffff7c7f4f77b04cecf18f5
ivmartel <ivmartel@gmail.com>
2019-05-21 00:10:07 +0200

    Update stable demo to v0.3.0

---
+++ demo/stable/precache-manifest.cee0d72fb6705e2634877c7de15f5fe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1e4fade5e6c669a29a0e922bd95a65",
+    "url": "/dwv-react/demo/stable/index.html"
+  },
+  {
+    "revision": "622385b208cab0eec3f6",
+    "url": "/dwv-react/demo/stable/static/css/main.68c023de.chunk.css"
+  },
+  {
+    "revision": "04e685c13f2c5476adc1",
+    "url": "/dwv-react/demo/stable/static/js/2.bde51f5c.chunk.js"
+  },
+  {
+    "revision": "622385b208cab0eec3f6",
+    "url": "/dwv-react/demo/stable/static/js/main.11422f78.chunk.js"
+  },
+  {
+    "revision": "9653790fbf10b5aa9de6",
+    "url": "/dwv-react/demo/stable/static/js/runtime~main.424b9d46.js"
+  }
+]);
