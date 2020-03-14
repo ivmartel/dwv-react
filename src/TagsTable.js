e32dec9d706dfe2b2d34a3afb4aea911a98ca29e
ivmartel <ivmartel@gmail.com>
2020-03-15 00:25:12 +0100

    Broader string type test.

--- src/TagsTable.js
+++ src/TagsTable.js
@@ -62,7 +62,7 @@ class TagsTable extends React.Component {
       for ( var key in item ) {
         if( item.hasOwnProperty(key) ) {
           var value = item[key];
-          if ( typeof value === "number" ) {
+          if ( typeof value !== "string" ) {
             value = value.toString();
           }
           if ( value.toLowerCase().indexOf(searchLo) !== -1 ) {
