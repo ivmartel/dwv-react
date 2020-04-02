c63caba757404ba6d9c0348f28d12ee5cd4b8106
Travis <travis@travis-ci.org>
2020-04-02 22:29:06 +0000

    Travis build 562 pushed to gh-pages

--- demo/trunk/precache-manifest.946aa040d6b03a4939ff822900e1191f.js
+++ demo/trunk/precache-manifest.946aa040d6b03a4939ff822900e1191f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dcdf4781b48c4291527c156fe96deee0",
+    "revision": "577340c38eb203d18fa941b88b166262",
     "url": "/dwv-react/demo/trunk/index.html"
   },
   {
-    "revision": "c0b9b89fd06a2ff3ee1f",
+    "revision": "3ac1d4b2e9f0bbd42cb5",
     "url": "/dwv-react/demo/trunk/static/css/main.68c023de.chunk.css"
   },
   {
-    "revision": "da824a24bd7a5dddcd2d",
-    "url": "/dwv-react/demo/trunk/static/js/2.21b15293.chunk.js"
+    "revision": "b9b7e2bc5dced4132cd3",
+    "url": "/dwv-react/demo/trunk/static/js/2.135e3f92.chunk.js"
   },
   {
     "revision": "c99ce4bb68ceed0b6d9cbb4e7cbdcc53",
-    "url": "/dwv-react/demo/trunk/static/js/2.21b15293.chunk.js.LICENSE.txt"
+    "url": "/dwv-react/demo/trunk/static/js/2.135e3f92.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c0b9b89fd06a2ff3ee1f",
-    "url": "/dwv-react/demo/trunk/static/js/main.d2737f11.chunk.js"
+    "revision": "3ac1d4b2e9f0bbd42cb5",
+    "url": "/dwv-react/demo/trunk/static/js/main.a082e79c.chunk.js"
   },
   {
     "revision": "3d8b5b503db2aa275991",
--- demo/trunk/service-worker.js
+++ demo/trunk/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dwv-react/demo/trunk/precache-manifest.3d610b0e4d63c4759e0a42305b721ad9.js"
+  "/dwv-react/demo/trunk/precache-manifest.946aa040d6b03a4939ff822900e1191f.js"
 );
 
 self.addEventListener('message', (event) => {
