32d499e7b2b1334254827575c71b63c10a2e8e50
ivmartel <ivmartel@gmail.com>
2021-02-13 18:13:24 +0100

    Add a publish job and a new update script.

---
+++ resources/scripts/update-gh-pages_github.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#Script to push build results on the repository gh-pages branch.
+
+# we should be in /home/travis/build/ivmartel/dwv-react
+echo -e "Starting to update gh-pages\n"
+
+pwd
+
+ls -l
+
+# setup git
+git config --global user.email github-actions@github.com
+git config --global user.name github-actions
+# using token, clone gh-pages branch
+git checkout gh-pages
+# clean up demo
+rm -Rf demo/trunk/*
+# copy new build in demo/trunk
+cp -Rf build/* demo/trunk
+# add, commit and push files
+git add demo/trunk/*
+echo "Github actions build ${github.run_id} pushed to gh-pages"
+
+#git commit -m "Github actions build ${github.run_id} pushed to gh-pages"
+#git push -fq origin gh-pages
+
+echo -e "Done updating.\n"
