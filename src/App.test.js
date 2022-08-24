ffead89462155bbfafad4c78c0d738f5675d0b1f
ivmartel <ivmartel@gmail.com>
2022-08-24 11:50:02 +0200

    Update react to v18, fixes #147

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const root = createRoot(div);
+  root.render(<App />);
 });
--- src/DwvComponent.test.js
+++ src/DwvComponent.test.js
@@ -1,34 +1,40 @@
 import React from 'react';
-import {render, unmountComponentAtNode} from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import {act} from 'react-dom/test-utils';
 import DwvComponent from './DwvComponent';
 
 describe('DwvComponent', () => {
   let container = null;
+  let root = null;
   beforeEach(() => {
+    // allow act()
+    globalThis.IS_REACT_ACT_ENVIRONMENT = true;
     // setup a DOM element as a render target
     container = document.createElement("div");
+    root = createRoot(container);
     document.body.appendChild(container);
   });
 
   afterEach(() => {
-    // cleanup on exiting
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
+    act(() => {
+      // cleanup on exiting
+      root.unmount();
+      container.remove();
+      container = null;
+    });
   });
 
   // Inspect the component instance on mount
   it('renders without crashing', () => {
     act(() => {
-      render(<DwvComponent />, container);
+      root.render(<DwvComponent />);
     });
   });
 
   // Mount an instance and inspect the render output
   it('renders the beginning of the legend', () => {
     act(() => {
-      render(<DwvComponent />, container);
+      root.render(<DwvComponent />);
     });
     const legend = container.querySelector('.legend');
     expect(legend.textContent).toContain('Powered by');
