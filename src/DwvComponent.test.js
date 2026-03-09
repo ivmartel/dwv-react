9654c9ca826402e1004b35e2729ee5603baee1a1
ivmartel <ivmartel@gmail.com>
2026-03-09 17:08:42 +0100

    Add service provider to tests

--- src/DwvComponent.test.js
+++ src/DwvComponent.test.js
@@ -1,16 +1,25 @@
 import {render, screens} from '@testing-library/react'
+import { DwvServiceProvider } from './DwvServiceProvider.jsx';
 
 import DwvComponent from './DwvComponent.jsx';
 
 describe('DwvComponent', () => {
   // Inspect the component instance on mount
   it('renders without crashing', () => {
-    render(<DwvComponent />);
+    render(
+      <DwvServiceProvider>
+        <DwvComponent />
+      </DwvServiceProvider>
+    );
   });
 
   // Mount an instance and inspect the render output
   it('renders the beginning of the legend', () => {
-    const { container } = render(<DwvComponent />);
+    const { container } = render(
+      <DwvServiceProvider>
+        <DwvComponent />
+      </DwvServiceProvider>
+    );
 
     const legend = container.querySelector('.legend');
     expect(legend.textContent).toContain('Powered by');
