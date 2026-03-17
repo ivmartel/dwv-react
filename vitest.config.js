0c75e4a1ead85ea01f5cc7de79fdf1f305d71838
ivmartel <ivmartel@gmail.com>
2026-03-17 12:11:19 +0100

    Remove mode unnecessary config

--- vitest.config.js
+++ vitest.config.js
@@ -18,24 +18,5 @@ export default defineConfig({
         lines: 10
       }
     }
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: { '.js': 'jsx' },
-      plugins: [
-        {
-          name: 'load-js-files-as-jsx',
-          setup(build) {
-            build.onLoad(
-              { filter: /(src)\/.*\.js$/ },
-              async (args) => ({
-                loader: 'jsx',
-                contents: await fs.readFile(args.path, 'utf8'),
-              })
-            );
-          },
-        },
-      ],
-    },
   }
 })
