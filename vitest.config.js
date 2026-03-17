25b43792509efad8adeee81661d5b3a72b63c0a7
ivmartel <ivmartel@gmail.com>
2026-03-17 12:04:52 +0100

    Remove unneeded config

--- vitest.config.js
+++ vitest.config.js
@@ -19,11 +19,6 @@ export default defineConfig({
       }
     }
   },
-  esbuild: {
-    loader: 'jsx',
-    include: /(src)\/.*\.jsx?$/,
-    exclude: [],
-  },
   optimizeDeps: {
     esbuildOptions: {
       loader: { '.js': 'jsx' },
